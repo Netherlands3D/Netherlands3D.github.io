18bfee6cb13064b4759a50a5a4f82d37bdf6b6a1
Mike van Riel <mike.vanriel@flevoland.nl>
2026-03-02 08:40:36 +0100

    Live reloading is broken in the latest mkdocs due to an upstream issue - using the --livereload option will make it work again as a workaround

--- bin/develop.sh
+++ bin/develop.sh
@@ -1,3 +1,3 @@
 # go one directory up so the mkdocs.yml file can be found
 cd .. 
-docker run --rm -it -p 8000:8000 -v $(pwd):/docs squidfunk/mkdocs-material
+docker run --rm -it -p 8000:8000 -v $(pwd):/docs squidfunk/mkdocs-material serve --livereload
